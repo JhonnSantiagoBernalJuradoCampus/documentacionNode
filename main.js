685fc5cd9d06dc6ff25160648cad52467288a3fe
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-07-01 09:44:22 -0500

    feat: :sparkles: importar con el require la funcion place

---
+++ main.js
@@ -0,0 +1,8 @@
+//main.js
+/**
+ * * Importa el "module.js" utilizando require() sin la extencion .js
+ * ? Crear archivo "main.js" para importar el "module.js"
+ */
+
+const config = require("./module");
+console.log(config.place());
