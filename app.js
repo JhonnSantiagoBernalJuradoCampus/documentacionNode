07d74be3e2113afae1f3189c6e76e8b293c2676f
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-07-02 12:42:04 -0500

    feat: :sparkles: Metodo fs.readdir
    Lee el contenido de un directorio de forma asincrónica
    
    recibe como parametros del callback error y array(files)

--- app.js
+++ app.js
@@ -1,55 +1,24 @@
 // Require fs
 /**
- * * fs.appendFileSync(file,data, options):
- * ? Agrega datos al final de un archivo de forma sincrónica (bloqueante)
- * @param file: Ruta del archivo al que se escribiran los datos
- * @param data: Datos a escribir en el archivo
- * @param options: Opciones de escritura, como la codificacion
+ * * fs.readdir(path,callback):
+ * ? Lee el contenido de un directorio de forma asincrónica
+ * @param path = ruta del directorio
+ * @param callback Función de devolución de llamada que recibe err (error) y files
+(array de nombres de archivos en el directorio).
 */
 let fs = require('fs');
 
-let data = {
-    campus: [
-        {
-            id:1,
-            name:"Apolo"
-        },
-        {
-            id:2,
-            name:"Artemis"
-        },
-        {
-            id:3,
-            name:"Sputnik"
-        }
-    ]
-}
-let add = {
-    messages: [
-        {
-            id:1,
-            message:"Hola"
-        },
-        {
-            id:2,
-            message:"Mundo"
-        },
-        {
-            id:3,
-            message:"Chao"
-        }
-    ]
-}
+fs.readdir(__dirname, (error,files)=>{
+    if (error){
+        console.log(error);
+    }
+    else{
+        files.forEach((val,id)=>{
+            console.log(val);
+        })
+    }
+})
 
-fs.writeFileSync("db/db2.json", JSON.stringify(data));
-console.log("Se ha escrito correctamente,\n");
-console.log(`Lo que se escribio fue: ${fs.readFileSync("db/db2.json")}`);
 
-fs.appendFileSync("db/db2.json", JSON.stringify(add));
-console.log(`Se ha agregado correctamente, el archivo ahora tiene este contenido:\n ${fs.readFileSync("db/db2.json")}`);
-/**
- * * El ejemplo tiene fallos por el echo de el formato json
- * ? Revisar
- */
 
 
