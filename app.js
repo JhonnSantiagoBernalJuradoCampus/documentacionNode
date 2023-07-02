a599122e5032649b45162b3cef652a96d74b8abe
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-07-02 18:41:34 -0500

    feat: :sparkles: Metodo fs.mkdirSync
    Crea un directorio de forma sincrona

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
 // Require fs
 /**
- * * fs.mkdir(path, options, callback):
- * ? Crea un directorio de forma asincrónica
+ * * fs.mkdirSync(path, options):
+ * ?  Crea un directorio de forma sincrónica (bloqueante).
  * @param path = ruta del directorio a crear
  * @param options = Opciones para la creación del directorio.
- * @param callback = Función de devolución de llamada que recibe err(error) cuando se completa la creación
 */
 const fs = require('fs');
 const path = require('path');
 
-fs.mkdir(path.join(__dirname, "src"), (error)=>{
-    if (error){
-        console.log(error);
-    }
-    else{
-        console.log('Directory createde successfully');
-    }
-});
+fs.mkdirSync(path.join(__dirname, "src"));
+console.log("El directorio se ha creado correctamente");
