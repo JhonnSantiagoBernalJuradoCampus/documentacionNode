9c7bcc93d98a2ed8887c7e52407f0bd6186f43c9
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-07-03 09:04:51 -0500

    feat: :sparkles: Recibir datos de una peticion HTTP

--- app.js
+++ app.js
@@ -1,32 +1,34 @@
 // http
 /**
- * * request.url
- * ? La url de la solicitud
- * * request.method
- * ? El metodo HTTP utilizado en la solicitud(GET,POST, etc)
- * * request.headers
- * ? Un obejto que contiene los encabezados de la solicitud
- * * request.params
- * ? Los parametros de ruta si estas utlizando un enrutador
- * * request.query
- * ? Los parametros de consulta si estan presentes en la URL
- * * request.body
- * ? El cuerpo de la solicitud (datos enviados en una solicitud POST o PUT)
+ * *Cuando se recibe una solicitud HTTP
+ * ?Los datos se envian en forma de fragmentos (chunks) 
  */
 
 import { createServer } from "http";
+import fs from 'fs';
+
 const http = createServer((req,res)=>{
     /**
-     * @var {req.headers} * Obtenemos los encabezados de la solicitud enviada
+     * *Construimos el dato que enviamos por la solicitud HTTP: 'POST, GET, PUT, DELETE'
+     * @var {req.on("data", (chunk)=>{...})}*
      */
-    console.log(`Origen de la solicitud "Consola: ${req.headers["user-agent"]}"`);
-    res.end(`Origen de la solicitud HTML: ${req.headers["user-agent"]}`);
+    let data = "";
+    req.on("data", (chunk)=>{
+        data +=chunk;
+    })
+    /**
+     * *Una vez terminado la construccion del dato que realizamos lo gardamos en un archivo .txt
+     * @var {req.on("end", ()=>{...})}
+     */
+    req.on("end", ()=>{
+        console.log(data);
+        res.write(data);
+        fs.writeFile("data.txt", data, (err)=>{
+            console.log(err);
+        })
+        res.end();
+    })
 })
-/**
- * * Servido de escucha habilitado http://127.222.06:5510/
- * @var {hostname} Dominio * Por defecto es localhost pero lo cambiamos al rango de ip de pruebas 127
- * @var {port} Puerto
- */
 const config = {
     hostname: '127.222.06',
     port: 5510
