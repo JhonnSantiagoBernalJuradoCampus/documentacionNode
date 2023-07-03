8fb70a7dec982b655abf94b89b649d239f958387
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-07-03 00:56:31 -0500

    feat: :sparkles: Ejercicio Dolares a euros
    Modulos

---
+++ conversor.js
@@ -0,0 +1,8 @@
+export const convertirDolaresAEUROS = (dolares)=>{
+    return dolares*0.92;
+}
+const convertirDolaresALibras = (dolares) =>{
+    return dolares*0.79;
+}
+export default convertirDolaresAEUROS;
+export {convertirDolaresALibras};
