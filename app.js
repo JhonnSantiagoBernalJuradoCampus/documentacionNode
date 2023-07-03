76e80f37f854090c6d11f49758a797d6a2cda093
JhonnSantiagoBernalJuradoCampus <jhonbernalcampus@gmail.com>
2023-07-03 00:34:02 -0500

    feat: :sparkles: Import, Export npm
    Importo las funciones trainers, salones, coordinadores para poder mostrarlos en consola desde el app.js

--- app.js
+++ app.js
@@ -1,28 +1,10 @@
-// Require http
+//app.js
 /**
- * * http.get(options, callback)
- * ?  Realiza una solicitud HTTP GET a través de HTTPS
- * @param options = Opciones de la solicitud, como la url y los encabezados
- * @param callback = funcion de devolucion de llamada que se ejecuta cuando se completa la respuesta.  Recibe el objeto de respuesta
- * * http.request(options, callback)
- * ? Inicia una solicitud http personalizada a través de HTTPS
- * @param options =  Opciones de la solicitud, como la URL, los encabezados y el método HTTP
- * @param callback = Funcion de devolucion de llamada que se ejecuta cuando se completa la respuesta. Recibe el objeto de respuesta
- * * http.createServer(options, requestListener)
- * ? Crea un servidor HTTPS
- * @param options = Opciones para la creacion del servidor, como el certificado y la clave privada
- * @param requestListener = Funcion que se ejecuta cuando se recibe una solicitud en el servidor
-*/
-
-const http = require('http');
-const requestListener = (req, res) =>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello World</h1>');
-}
-
-const server = http.createServer(requestListener);
-
-server.listen(3000, "localhost", ()=>{
-    console.log("El servidor esta escuchando en http://localhost:3000/");
-})
+ * ?Para importar los modulos de las dos maneras que exportamos son:
+ * @var {export default} = trainer
+ * @var {export} = {coordinadores, salones}
+ */
+import trainers, {coordinadores, salones} from './main.js';
+console.log(`Trainers: ${trainers().join(", ")}`);
+console.log(`Salones:`, salones());
+console.log(`Coodinadores: `, coordinadores());
---
+++ main.js
@@ -0,0 +1,12 @@
+//main.js
+export const trainers = () =>{
+    return ["Santiago", "Angie", "Pedre", "Miguel", "Valentina"];
+}
+const coordinadores = () =>{
+    return ["Karen", "Juan"];
+}
+const salones = () =>{
+    return ["Apolo","Artemis","Sputnik"];
+}
+export default trainers;
+export {coordinadores, salones};
